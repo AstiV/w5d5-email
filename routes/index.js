fbd5674af327cfc105a914ba4329735fcec45111
Astrid Varga <astrid.varga@gmx.de>
2018-08-31 11:53:48 +0200

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+const Victim = require("../models/Victim");
+const { sendMail } = require("../utils/email");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+router.post("/email", (req, res, next) => {
+    new Victim({ email: req.body.email })
+        .save()
+        .then(victim => {
+            const htmlString = fs
+                .readFileSync(path.join(__dirname, "../views/emails/phishing.html"))
+                .toString();
+
+            return sendMail(victim.email, "Welcome to Ironhack!", undefined, htmlString);
+        })
+        .then(() => {
+            res.send("Thanks for participating");
+        });
 });
 
 module.exports = router;
---
+++ utils/email.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "lukas.ironhack@gmail.com",
+        pass: "ox99abb5d62ba94b51aa60b4d2"
+    }
+});
+
+function sendMail(to, subject, text, html) {
+    return new Promise((resolve, reject) => {
+        const data = {
+            from: "lukas.ironhack@gmail.com",
+            to,
+            subject,
+            text,
+            html
+        };
+        transporter
+            .sendMail(data)
+            .then(info => {
+                console.log(info);
+                resolve({ success: true });
+            })
+            .catch(error => {
+                console.log(error);
+                resolve({ success: false });
+            });
+    });
+}
+
+module.exports = {
+    sendMail
+};
